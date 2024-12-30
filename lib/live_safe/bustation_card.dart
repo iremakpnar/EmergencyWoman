5608f05c697d172b58f9148cab8c102d61953abd
Nlyfdnc <115823900+Nlyfdnc@users.noreply.github.com>
2024-12-30 23:59:23 +0300

    Hastane,Eczane,Durak,Polis Merkezi haritalarının eklenmesş

--- lib/live_safe/bustation_card.dart
+++ lib/live_safe/bustation_card.dart
@@ -1,25 +1,31 @@
 import 'package:flutter/material.dart';
 
 class BustationCard extends StatelessWidget {
-  const BustationCard({super.key});
+  final Function? onMapFunction;
+  const BustationCard({super.key, this.onMapFunction});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 10,),
+      padding: const EdgeInsets.only(left: 20,),
       child: Column(
         children: [
-          Card(
-            elevation: 6.0,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20)
-            ),
-            child: Container(
-              height: 50,
-              width: 50,
-              child: Center(
-                child: Image.asset('icons/bus_stop.png',
-                  height: 32,
+          InkWell(
+            onTap: (){
+              onMapFunction!('bus stations near me');
+            },
+            child: Card(
+              elevation: 6.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)
+              ),
+              child: Container(
+                height: 60,
+                width: 60,
+                child: Center(
+                  child: Image.asset('icons/bus_stop.png',
+                    height: 50,
+                  ),
                 ),
               ),
             ),
--- lib/widget/live_safe.dart
+++ lib/widget/live_safe.dart
@@ -3,9 +3,22 @@ import 'package:emergencywoman/live_safe/bustation_card.dart';
 import 'package:emergencywoman/live_safe/hospital_card.dart';
 import 'package:emergencywoman/live_safe/pharmacy_card.dart';
 import 'package:emergencywoman/live_safe/police_stationCard.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LiveSafe extends StatelessWidget {
   const LiveSafe({super.key});
+
+  static Future<void> openMap(String location) async {
+    String googleUrl="https://www.google.com/maps/search/$location";
+    final Uri _url = Uri.parse(googleUrl);
+
+    try {
+      await launchUrl(_url);
+    } catch (e){
+      Fluttertoast.showToast(msg: 'something went wrong! call emergency number');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,10 +29,10 @@ class LiveSafe extends StatelessWidget {
         physics: BouncingScrollPhysics(), //kaydırma sırasında zıplama sağlar
         scrollDirection: Axis.horizontal, // kaydırma yönü
         children: [
-             PoliceStation(),
-             HospitalCard(),
-             PharmacyCard(),
-             BustationCard(),
+          PoliceStation(onMapFunction: openMap,),
+          HospitalCard(onMapFunction: openMap,),
+          PharmacyCard(onMapFunction: openMap,),
+          BustationCard(onMapFunction: openMap,),
         ],
       ),
     );
