ccaf88c90306cadbe3cbe0b3cdb1494d81bbd76b
iremA <iirem.akpnarr@gmail.com>
2025-01-03 11:03:15 +0300

    Kadın sığınma evlerinin konumları, bottomnav, profil sayfası ve profil ayarları

---
+++ lib/settings/todolist.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ChangePasswordPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Şifre Değiştir")),
+      body: Center(child: Text("Şifre değiştirme ekranı.")),
+    );
+  }
+}
